bd870f133a79eff82b095e79048fed8dd8b98925
DIAKITE SOUMAILA <soumdiakite182@gmail.com>
2020-06-17 10:49:34 +0000

    Project init

---
+++ lce_immob/home.php
@@ -0,0 +1,8 @@
+<!-- BEGIN MENU -->
+<?php include_once('include/header.php'); ?>
+<!-- END MENU -->
+<!-- Start slider -->
+
+
+
+<?php include_once('../include/footer.php') ?>
---
+++ lce_immob/include/header.php
@@ -0,0 +1,195 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>LCE Sarl</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/icon" href="../assets/images/fac.ico"/>
+    <!-- Font Awesome -->
+    <link href="../assets/css/font-awesome.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <!-- Slick slider -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/slick.css"/>
+    <!-- Fancybox slider -->
+    <link rel="stylesheet" href="../../assets/css/jquery.fancybox.css" type="text/css" media="screen" />
+    <!-- Animate css -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/animate.css"/>
+    <!-- Progress bar  -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap-progressbar-3.3.4.css"/>
+    <!-- Theme color -->
+    <link id="switcher" href="../assets/css/theme-color/default-theme.css" rel="stylesheet">
+
+    <!-- Main Style -->
+    <link href="../style.css" rel="stylesheet">
+
+    <!-- Fonts -->
+
+    <!-- Open Sans for body font -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <!-- Lato for Title -->
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+
+<!-- BEGAIN PRELOADER -->
+
+
+
+
+<!-- END PRELOADER -->
+
+<!-- SCROLL TOP BUTTON -->
+<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+<!-- END SCROLL TOP BUTTON -->
+
+<!-- Start header -->
+<header id="header" class=" navbar-fixed-top" >
+    <!-- header top search -->
+
+    <!-- header bottom -->
+    <div class="header-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7 col-sm-7 col-xs-7">
+                    <div class="header-contact">
+                        <ul>
+                            <li>
+                                <div class="phone" >
+                                    <i class="fa fa-phone"></i>
+                                    <a href=" +225 46 66 77 66" style="color:#f45d00;"> +225 46 66 77 66</a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="mail">
+                                    <i class="fa fa-envelope"></i>
+
+                                    <a href="locomotive@gmail.com" style="color:#f45d00;">locomotive@gmail.com</a>
+                                </div>
+                            </li>
+
+
+
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</header>
+<!-- End header -->
+
+
+<!-- BEGIN MENU -->
+<section id="menu-area" >
+
+
+   <div class="navbar navbar-default navbar-fixed-top" style="border-bottom:4px solid #fbbc2f;" style="border-right:6px solid black;">
+
+     <div class="header-bottom" style="background-color: #ffaa01;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7 col-sm-7 col-xs-7">
+                    <div class="header-contact">
+                        <ul>
+                            <li>
+                                <div class="phone" >
+                                    <i class="fa fa-phone" style="color:white;"></i>
+                                  <a href="#" style="color:white;"><b> +225 46 66 77 66</b></a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="mail">
+                                    <i class="fa fa-envelope" style="color:white;"></i>
+                                    
+                                    <a href="locomotive@gmail.com" style="color:white;"><b>locomotivesarl@gmail.com</b></a>
+                                </div>
+                            </li>
+                            
+                            
+                            
+                        </ul>
+                    </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    </div>
+         <div class="container">
+          
+
+   
+            <div class="navbar-header">
+                <!-- FOR MOBILE VIEW COLLAPSED BUTTON -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- LOGO -->
+                <!-- TEXT BASED LOGO -->
+                   <a href="../index.php"><img src="../assets/images/logo.png" alt="logo"></a> 
+        
+                <!-- IMG BASED LOGO  -->
+               </div>
+            <div id="navbar" class="navbar-collapse collapse"  >
+                
+             <ul id="top-menu" class="nav navbar-nav navbar-right main-nav">
+                <li class="active" style="color:black;"><a href="index.php"><b>ACCUEIL</b></a></li>
+                    
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color:black;"><b>Activit&eacute;s </b><span class="fa fa-angle-down"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                            <li><a href="left.php" style="color: "><b>NOS Solutions</b></a></li>
+                            <li><a href="portfolio.php" ><b>Location de vehicules</b></a></li>
+                           
+                            <li><a href="service.php" ><b>Formation et Securit&eacute;</b></a></li>
+                            
+                            <li><a href="web.php" ><b>Developpement web</b></a></li>
+                            <li><a href="prestation.php" ><b>Prestations de Services Diverses</b></a></li>
+                             <li><a href="elctricité.php" ><b>Tous Travaux de BTP,ICT,&eacute;lectricit&eacute;</b></a></li>
+                            <li><a href="telecom.php" ><b>Expertise Informatique et Telecoms</b></a></li>
+                            
+                    </ul> 
+
+                </li>
+                    <li class="dropdown">
+                       <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color:black;"><b>Actualit&eacute;s  </b><span class="fa fa-angle-down"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="seminaire.php"><b>NOS SEMINAIRES<i style="color:white;"></i></b></a></li>
+                            <li><a href="realisations.php"><b>NOS REALISATIONS</b></a></li>
+
+                            
+                        </ul>
+                    </li>
+                   
+                    <li><a href="contact.php"><b>CONTACTEZ-NOUS</b></a></li>
+
+
+
+
+                 </ul>
+            </div>
+        </div>
+    </nav>
+</section><br/><br/><br/>
+<!-- END MENU -->
