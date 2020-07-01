e581eb65db31f59979363e2457a08effc3bdffd2
excellenceK <thierrykouamek1@gmail.com>
2020-07-01 17:39:31 +0000

    telecoms page 50% finish

---
+++ telecoms.php
@@ -0,0 +1,119 @@
+<?php include_once('include/header.php'); ?>
+
+<section id="thinks" class="soft_dev section-bg">
+    <div class="container">
+        <div class="section-title">
+            <h2 data-aos="fade-in" class="aos-init">Think</h2>
+        </div>
+        <div class="row content" id="">
+            <div class="col-md-5 aos-init" data-aos="fade-right">
+                <img src="assets/img/telecoms/cablage.jpg" alt="Image solution 3" class="img-fluid img-thumbnail">
+            </div>
+            <div class="col-md-7 pt-7 aos-init" data-aos="fade-left">
+                <h3>Design/Engineering</h3>
+                <p style="color: black; font-size: 18px;text-align: justify;line-height: 35px;">
+                Le paysage des données et des télécommunications continue d'évoluer à un rythme rapide et une planification compétente, stratégique et minutieuse est nécessaire pour prendre aujourd'hui les décisions qui amélioreront la fiabilité, l'efficacité et la durabilité des services de données et de télécommunications.
+                C'est pourquoi notre équipe d'experts se charge d'élaborer des plans créatifs et adptés aux besoins pour le bonheur de nos clients.
+                    </p>
+                <p style="color: black; font-size: 18px;text-align: justify;line-height: 35px;">
+                Nos design Télécom sont innovants et adptés aux besoins.
+                </p>
+                <p style="color: black; font-size: 18px;text-align: justify;line-height: 35px;">
+                   Pour tous travaux de <b>Design</b> de Plan Réseaux, Contactez nous <a href="contacts.php">ici</a>.
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section id="build" class="digit_trans section-bg">
+    <div class="container">
+        <div class="section-title">
+            <h2 data-aos="fade-in" class="aos-init">Build</h2>
+        </div>
+        <div class="row content" id="">
+            <div class="col-md-5 aos-init" data-aos="fade-right">
+                <img src="assets/img/telecoms/build.jpg" alt="Image solution 3" class="img-fluid img-thumbnail">
+            </div>
+            <div class="col-md-7 pt-7 aos-init" data-aos="fade-left">
+                <h3>Rollout/Upgrade/downgrade</h3>
+                <p style="color: black; font-size: 18px;text-align: justify;line-height: 35px;">
+                L'arrivée de la 5G entraîne de nouvelles opportunités massives pour l'industrie des télécommunications. 
+                Mais comme les entreprises investissent massivement pour créer une densité de réseau plus élevée, ajouter du spectre et mettre à niveau l'équipement actif, 
+                elles doivent réfléchir soigneusement aux capacités dans lesquelles elles devraient investir pour réussir à long terme. 
+                </p>
+                <p style="color: black; font-size: 18px;text-align: justify;line-height: 35px;">
+                Nous vous aiderons non seulement à concevoir votre avenir, mais nous le <b>construirons</b> également avec vous de manière tangible et pratique.                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section id="operate" class="digit_trans section-bg">
+    <div class="container">
+        <div class="section-title">
+            <h2 data-aos="fade-in" class="aos-init">Operate</h2>
+        </div>
+        <div class="row content" id="">
+            <div class="col-md-5 aos-init" data-aos="fade-right">
+                <img src="assets/img/telecoms/build.jpg" alt="Image solution 3" class="img-fluid img-thumbnail">
+            </div>
+            <div class="col-md-7 pt-7 aos-init" data-aos="fade-left">
+                <h3>NOC/FLM/NPM</h3>
+                <p style="color: black; font-size: 18px;text-align: justify;line-height: 35px;">
+                L'arrivée de la 5G entraîne de nouvelles opportunités massives pour l'industrie des télécommunications. 
+                Mais comme les entreprises investissent massivement pour créer une densité de réseau plus élevée, ajouter du spectre et mettre à niveau l'équipement actif, 
+                elles doivent réfléchir soigneusement aux capacités dans lesquelles elles devraient investir pour réussir à long terme. 
+                </p>
+                <p style="color: black; font-size: 18px;text-align: justify;line-height: 35px;">
+                Nous vous aiderons non seulement à concevoir votre avenir, mais nous le <b>construirons</b> également avec vous de manière tangible et pratique.                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section id="support" class="digit_trans section-bg">
+    <div class="container">
+        <div class="section-title">
+            <h2 data-aos="fade-in" class="aos-init">Support</h2>
+        </div>
+        <div class="row content" id="">
+            <div class="col-md-5 aos-init" data-aos="fade-right">
+                <img src="assets/img/telecoms/build.jpg" alt="Image solution 3" class="img-fluid img-thumbnail">
+            </div>
+            <div class="col-md-7 pt-7 aos-init" data-aos="fade-left">
+                <h3>Remote/On site/Audit</h3>
+                <p style="color: black; font-size: 18px;text-align: justify;line-height: 35px;">
+                L'arrivée de la 5G entraîne de nouvelles opportunités massives pour l'industrie des télécommunications. 
+                Mais comme les entreprises investissent massivement pour créer une densité de réseau plus élevée, ajouter du spectre et mettre à niveau l'équipement actif, 
+                elles doivent réfléchir soigneusement aux capacités dans lesquelles elles devraient investir pour réussir à long terme. 
+                </p>
+                <p style="color: black; font-size: 18px;text-align: justify;line-height: 35px;">
+                Nous vous aiderons non seulement à concevoir votre avenir, mais nous le <b>construirons</b> également avec vous de manière tangible et pratique.                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<br><br>
+
+<div class="container mb-3">
+    <div class="row">
+        <div class="bx-pull-right">
+            <a type="button" class="btn btn-outline-primary"
+               href="clients_project.php"
+            >Avez-vous un projet ?</a>
+        </div>
+        <div class="bx-pull-right">
+            <a type="button" class="btn btn-outline-secondary"
+               href="contacts.php"
+            >Contactez - Nous !</a>
+        </div>
+    </div>
+</div>
+<br>
+
+<?php include_once('include/footer.php') ?>
