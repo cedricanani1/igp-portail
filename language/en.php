cfaee5d1f31865e288255781ae31679392e31e38
DIAKITE SOUMAILA <soumdiakite182@gmail.com>
2020-07-10 11:33:52 +0000

    Internationalization init

---
+++ language/en.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang = array(
+    "title" => "Hello, world!",
+    "menu_1" => "Home",
+    "menu_2" => "Contact",
+    "title_home" => "My amazing website",
+    "desc_home" => "This is explanation about my amazing website!",
+    "contact_home" => "Contact",
+    "desc_cont" => "Contact me for your requests"
+);
---
+++ language/fr.php
@@ -0,0 +1,171 @@
+<?php
+
+$lang = array(
+    "header" => array(
+        "h1" => "Accueil",
+        "h2" => "Nos solutions",
+        "h2_" => array(
+            "1" => "IT",
+            "1_" => array(
+                "1" => "Web master",
+                "2" => "transformation digital",
+                "3" => "SaaS",
+                "4" => "Cloud computing",
+                "5" => "réseautage d'entreprise",
+                "6" => "Business intelligence",
+                "7" => "Cyber sécurité",
+                "8" => "IOT/IA",
+                "9" => "Réalité Virtuelle",
+            ),
+            "2" =>  "Télécom",
+            "2_" => array(
+                "1" => "think",
+                "2" => "build",
+                "3" => "operate",
+                "4" => "support",
+                "5" => "develop"
+            ),
+            "3" => "Logistique",
+            "4" => "BTP",
+            "5" => "Formations",
+        ),
+        "h3" => "Boutique",
+        "h4" => "A propos",
+        "h4_" => array(
+            "1" => "LCE",
+            "2" => "Partenaires",
+            "3" => "Carrières",
+            "4" => "Galerie",
+            "5" => "Contacter nous"
+        ),
+    ),
+    "footer" => array(
+        "1" => "Accès rapide",
+        "1_" => array(
+            "1" => "Accueil",
+            "2" => "Nos solutions",
+            "3" => "A propos",
+            "4" => "Contacts"
+        ),
+        "2" => "Client & Partenaires",
+        "2_" => array(
+            "1" => "Solution partners",
+            "2" => "Channel partner",
+            "3" => "Service partner"
+        ),
+        "3" => "Support",
+        "3_" => array(
+            "1" => "B2B Support",
+            "2" => "B2C Support",
+            "3" => "B2E Support",
+            "4" => "Bulletin de sécurité"
+        ),
+        "4" => "Presse et événements",
+        "4_" => array(
+            "1" => "News",
+            "2" => "Events",
+            "3" => "Nos réalisations",
+            "4" => "Photos galerie"
+        ),
+        "5" => "Portails",
+        "5_" => array(
+            "1" => "Carrières",
+            "2" => "Développeurs",
+            "3" => "Fournisseurs",
+            "4" => "Blog LCE"
+        ),
+    ),
+    "home" => array(
+        "" => "",
+        "" => "",
+        "" => "",
+        "" => ""
+    ),
+    "solutions" => array(
+        "it" => array(
+            "webmaster" => "",
+            "digit_trans" => "",
+            "saas" => "",
+            "cloud_c" => "",
+            "enter_net" => "",
+            "bi" => "",
+            "cyber_sec" => "",
+            "iot" => "",
+            "vr" => "",
+        ),
+        "telecoms" =>  array(
+            "think" => "",
+            "build" => "",
+            "operate" => "",
+            "support" => "",
+            "develop" => ""
+        ),
+        "logistique" => "",
+        "btp" => "",
+        "formation" => "",
+    ),
+    "boutique" => array(
+        "" => "",
+        "" => "",
+        "" => "",
+        "" => ""
+    ),
+    "about" => array(
+        "lce" =>  array(
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+        ),
+        "partners" =>  array(
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+        ),
+        "carreers" =>  array(
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+        ),
+        "gallery" =>  array(
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+        ),
+        "contact" =>  array(
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+            "" => "",
+        ),
+    ),
+
+);
